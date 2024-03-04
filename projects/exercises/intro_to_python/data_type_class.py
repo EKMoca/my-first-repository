cd5da26bd6e5cfd96227470cf281e25f9804de59
EKMoca <organelk@gmail.com>
2024-03-04 21:25:44 +0000

    first chapter exercises

---
+++ projects/exercises/intro_to_python/data_type_class.py
@@ -0,0 +1,33 @@
+# Identify the data type or class for each of the following values:
+
+'True' == str()
+print(type('True'))
+
+False == bool()
+print(type(False))
+
+my_tuple = (1, 2, 3,)
+print(type(my_tuple))
+
+1.5 == float()
+print(type(1.5))
+
+[1, 2, 3,] == list()
+print(type([1, 2, 3,]))
+
+2 == int()
+print(type(2))
+
+barnacles = range(5)
+print(type(barnacles))
+
+{1, 2, 3} == set()
+print({1, 2, 3})
+
+None == None
+print(type(None))
+
+{'foo': 'bar'}
+print(type({'foo': 'bar'}))
+
+
