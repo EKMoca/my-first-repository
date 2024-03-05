a2a8c8d52861b22f01574dff6ad9640f56abca49
EKMoca <organelk@gmail.com>
2024-03-05 22:11:12 +0000

    new exercises

---
+++ projects/exercises/intro_to_python/basic_operations.py
@@ -0,0 +1,12 @@
+#Concatenate two strings, one with your first name and one with your last, 
+#to create a new string with your full name as its value.
+
+full_name = 'Erik ' + 'Mojica'
+print(full_name)
+
+
+print(4936 % 10)
+print(((4936 % 100) - (4936 % 10)))
+print(((4936 % 1000) - (4936 % 100)))
+print(((4936 % 10000) - (4936 % 1000)))
+
--- projects/exercises/intro_to_python/data_type_class.py
+++ projects/exercises/intro_to_python/data_type_class.py
@@ -30,4 +30,25 @@ print(type(None))
 {'foo': 'bar'}
 print(type({'foo': 'bar'}))
 
+#Create a tuple called names that contains several pet names. For instance:
+names = (
+    'Asta',
+    'Butterscotch',
+    'Pudding',
+    'Neptune',
+    'Darwin',
+)
+print(names)
+
+#Create a dictionary named pets that contains a list of pet names and the type of animal. For instance:
+
+pets = {
+    'Asta':         'dog',
+    'Butterscotch': 'cat' ,
+    'Pudding':      'cat',
+    'Neptune':      'fish',
+    'Darwin':       'lizard',
+}
+
+print(pets)
 
