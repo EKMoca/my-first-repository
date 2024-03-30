6fbe268c06551dc3a9cdcac8b96dbc482fc1ecc5
EKMoca <organelk@gmail.com>
2024-03-30 19:08:34 +0000

    organized exercises to directories

--- projects/exercises/intro_to_python/input_output/greeter.py
+++ projects/exercises/intro_to_python/input_output/greeter.py
@@ -6,3 +6,12 @@ greetings = ['good morning,', 'good afternoon,', 'good evening,']
 for i in greetings:
     print(i, first_name + '.')
     
+name = 'January Fooze'
+print(f'Nice to meet you, {name}!')
+
+
+import time
+print(time.asctime())
+
+print(1, 2, 3, False, {'apple', 'crackle', 'pop'}, sep='')
+print('a', 'b', end='', sep=','); print('c', 'd', sep=',')
---
+++ projects/exercises/intro_to_python/loops_iterating/counter.py
@@ -0,0 +1,9 @@
+# Chapter: Collections and Iteration
+# sub-chapter, Loops and Iterating
+
+counter = 2
+while counter <= 100:
+    print(counter) # iterates over the block for all integer 
+    counter += 8   # values between 1 and 10, inclusive. 
+# block must modify the object counter, or else Infinite Loop.
+# each block run is called an 'iteration'.
